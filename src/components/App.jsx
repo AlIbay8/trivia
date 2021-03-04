584b183775878e345eba91cb07c1c4b590f7e1a1
AlIbay8 <43456952+AlIbay8@users.noreply.github.com>
2021-03-03 17:58:31 -0800

    day 3, goal 1 working on day 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,46 +7,67 @@ function Question(props) {
     <div>
       {props.question}
       <div className="answers">
-        {
-          props.answers?.map((answer) => {
-            console.log(answer)
-            return(
-              <Answer choice={answer} />
-            );
-          })
-        }
+        {props.answers?.map((answer) => {
+          return <Answer choice={answer} handleClick={props.handleClick}/>;
+        })}
       </div>
     </div>
   );
 }
 
 function Nextquestion(props) {
-  return(<button>Next Question</button>);
+  return <button onClick={props.handleClick}>Next Question</button>;
 }
 
 function Answer(props) {
-  console.log("Answers: ", props.answers);
-  return(
-    <div>{props.choice}</div>
-  );
+  return <div onClick={props.handleClick}>{props.choice}</div>;
 }
+
 
 function App() {
   // usestate here
-  let [isAnswered , setIsAnswered] = useState("unanswered");
-  
-    
+  let [isAnswered, setIsAnswered] = useState("unanswered");
+  let [questionNumber, setQuestionNumber] = useState(0);
+  // state variable for user choice here
+  let [answerChoice, setAnswerChoice] = useState(0);
+
+
   return (
     <div className="app">
-      Trivia! 
-      <Question question={data[0].question.text} answers={data[0].question.choices} />
-      <Answer answers={data[0].question.choices} />
-      <button onClick={() => setIsAnswered(data[0].question.choices[data[0].question.correct_choice_index])}>Answer Question</button>
+      Trivia!
+      <Question
+        question={data[questionNumber].question.text}
+        answers={data[questionNumber].question.choices}
+        handleClick={() => newAnswerChoice()}
+      />
+      {/*<Answer answers={data[questionNumber].question.choices} />*/}
+      <button
+        onClick={() =>
+          setIsAnswered(
+            data[questionNumber].question.choices[data[questionNumber].question.correct_choice_index]
+          )
+          
+        }
+      >
+        Answer Question
+      </button>
       <div>The correct answer is {isAnswered}</div>
-      <Nextquestion />
+      <Nextquestion handleClick={() => nextQuestion()}/>
     </div>
-    
   );
+
+  function nextQuestion() {
+    if (questionNumber < data.length-1) {
+      setQuestionNumber(questionNumber + 1);
+    } else {
+      setQuestionNumber(0);
+    }
+  }
+
+  // new function for when user clicks on a choice here
+  function newAnswerChoice(choice) {
+    console.log("clicked", choice);
+  }
 }
 
 export default App;
